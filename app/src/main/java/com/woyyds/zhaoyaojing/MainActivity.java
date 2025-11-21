661bfc52312064d266bd28d955a41cf08f244c2d
刘亚锋 <15975652+liuyafengyyds@user.noreply.gitee.com>
2025-11-21 11:45:16 +0800

    首次提交

--- app/src/main/java/com/woyyds/zhaoyaojing/MainActivity.java
+++ app/src/main/java/com/woyyds/zhaoyaojing/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
         webview = new WebView(this);
         webview.getSettings().setJavaScriptEnabled(true);
         webview.setWebViewClient(new WebViewClient());
-        webview.loadUrl("https://woyyds.com");
+        webview.loadUrl("https://woyyds.com/vue");
         setContentView(webview);
     }
 
